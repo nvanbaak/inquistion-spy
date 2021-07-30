e8c17ce0b1741b6e7f3f8d05a4f239a451ff7c7f
Nikolai <nvanbaak@gmail.com>
2021-07-30 10:10:44 -0700

    added characters

---
+++ character.py
@@ -0,0 +1,30 @@
+from random import randrange as roll
+
+class Character:
+    def __init__(self, char_name):
+        self.name = char_name
+        self.char_class = None
+    
+        self.char_stats = {
+            "WS" : 0,
+            "BS" : 0,
+            "S" : 0,
+            "T" : 0,
+            "Agi" : 0,
+            "Int" : 0,
+            "Wil" : 0,
+            "Fel" : 0
+        }
+
+    def generate_characteristics(self, mode):
+        if mode == "random" or mode == 0:
+            for stat in self.char_stats:
+                die_roll = roll(1,11) + roll(1,11)
+                self.char_stats[stat] = die_roll + 25
+        else:
+            for stat in self.char_stats:
+                self.char_stats[stat] = 25
+        return
+
+    def print_stats(self):
+        return self.char_stats
