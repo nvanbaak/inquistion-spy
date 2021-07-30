fc2abf27369345a6b7f2d3269ef174302276a6e1
Nikolai <nvanbaak@gmail.com>
2021-07-30 10:02:49 -0700

    added character object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
-from discord import message
 import config
+
+from character import Character
 
 client = discord.Client()
 
@@ -12,5 +13,23 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith("$"):
         print(message)
+
+    content = message.content
+    channel = message.channel
+
+    if content.startswith("$create"):
+        content = content.replace("$create ", "")
+        if content == "":
+            content = "New Guy"
+
+        new_guy = Character(content)
+        new_guy.generate_characteristics(0)
+
+        await channel.send(new_guy.print_stats())
+    
+    if content.startswith("$exterminatus"):
+        await channel.purge()
+        await channel.send("*The channel has been purged.*")
+
 
 client.run(config.bot_token)
