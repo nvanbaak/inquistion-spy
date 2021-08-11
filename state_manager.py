9ee35a6aecaf16111f84e04a4171e69997f1f4f8
Nikolai <nvanbaak@gmail.com>
2021-08-11 15:26:47 -0700

    if there's an am/pm designation,  updates accordingly

--- state_manager.py
+++ state_manager.py
@@ -102,8 +102,21 @@ class State_Manager:
                 time_value = "0" + time_value
             elif len(time_value) < 3 or len(time_value) > 4:
                 await channel.send("Your time formatting is heretical!  Proper servants of the emperor use three or four digits for times!")
+                return
 
-            await channel.send(time_value)
+            content_no_spaces = content.replace(" ", "")
+
+            # if there's an am or pm designation, update time accordingly
+            time_code = content_no_spaces[index:index+2]
+            if time_code.lower() == "am":
+                content_no_spaces = content_no_spaces[index+2:]
+            elif time_code.lower() == "pm":
+                time_value = int(time_value) + 1200
+                content_no_spaces = content_no_spaces[index+2:]
+
+            
+
+            await channel.send(time_code)
 
 
         elif content.startswith("$roll"):
