faa493befa4f79dde37d368d6398776d5b1dd79f
Nikolai <nvanbaak@gmail.com>
2021-08-12 15:06:15 -0700

    refactored client, added abilit to mute bot

--- main.py
+++ main.py
@@ -2,23 +2,41 @@ import discord
 import config
 import re
 
-from character import Character
 from utility.state_manager import State_Manager
 
-client = discord.Client()
-sm = State_Manager()
+class MyClient(discord.Client):
+    def __init__(self, *, loop=None, **options):
+        super().__init__(loop=loop, **options)
 
-binary_check = re.compile("[0-1| ]*")
+        self.sm = State_Manager()
 
-@client.event
-async def on_ready():
-    print('Bot successfully logged in as: {user}'.format(user=client.user))
+        self.binary_check = re.compile("[0-1| ]*")
+        self.env = config.env
+        self.muted = False
+    
+    async def on_ready(self):
+        print('Bot successfully logged in as: {user}'.format(user=client.user))
 
-@client.event
-async def on_message(message):
-    is_binary = binary_check.fullmatch(message.content)
-    if message.content.startswith("$") or is_binary:
-        print(message)
-        await sm.handle_command(client, message, is_binary)
+    async def on_message(self, message):
+        # this code lets us have multiple instances of the bot running, but choose which one responds for debugging purposes
+        if message.content.startswith("$set env "):
 
+            self.muted = self.env == config.env
+            self.env = message.content.replace("$set env", "").replace(" ", "")
+
+            if self.muted and self.env != config.env:
+                await message.channel.send("- Muting {}.".format(config.env))
+
+            elif not self.muted and self.env == config.env:
+                await message.channel.send("- Unmuting {}.".format(config.env))
+            return
+
+        if not self.muted:
+            is_binary = self.binary_check.fullmatch(message.content)
+            if message.content.startswith("$") or is_binary:
+                print(message)
+                await self.sm.handle_command(client, message, is_binary)
+
+
+client = MyClient()
 client.run(config.bot_token)
